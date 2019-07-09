c558fb677d66de3bdff7fdf34aae46836d95ad82
kartik <kartik.mathur10@gmail.com>
2019-07-09 09:14:03 +0530

    Lecture-14 & 15

--- Lecture-13/DynamicMemory.cpp
+++ Lecture-13/DynamicMemory.cpp
@@ -1,21 +1,35 @@
 #include <iostream>
 using namespace std;
+int* CreateArr(int n){
+	int *arr = new int[n]; // run time memory allocation
+
+	for(int i=0;i<n;i++){
+		arr[i] = i;
+	}	
+	return arr;
+}
 
 int main(){
 	int *a = new int;
 	*a = 10;
 
 	cout<<*a<<endl;
+	delete a;
+	a= NULL;
 	int n;
 	cin>>n;
-	int *arr = new int[n]; // run time memory allocation
 
-	for(int i=0;i<n;i++){
-		arr[i] = i;
-	}
+	// int *arr = new int[n]; // run time memory allocation
+
+	// for(int i=0;i<n;i++){
+	// 	arr[i] = i;
+	// }
+	int *arr = CreateArr(n);
 	for(int i=0;i<n;i++){
 		cout<<arr[i]<<" ";
 	}
+	delete []arr;
+	arr =NULL;
 	cout<<endl;
 
 
---
+++ Lecture-14/OOPS.cpp
@@ -0,0 +1,199 @@
+#include<iostream>
+using namespace std;
+
+//---------------BLUE PRINT --------------------
+class car
+{
+
+	int seats;
+public:
+	char *name;
+	int model;
+	int price;
+	
+
+	car()
+	{
+		cout<<"IAM IN DEFAULT CONSTRUCTOR"<<endl;
+		name=NULL;
+	}
+
+	car(char *ch,int m,int p,int s)
+	{
+		cout<<"Iam in 4 parameter CONSTRUCTOR"<<endl;
+		name=new char[strlen(ch)+1];
+		strcpy(name,ch);
+		model=m;
+		price=p;
+		seats=s;
+
+	}
+
+	car(char *ch,int m,int p)
+	{
+		cout<<"In 3 parameter CONSTRUCTOR"<<endl;
+		name=new char[strlen(ch)+1];
+		strcpy(name,ch);
+		model=m;
+		price=p;
+		seats=4;
+
+	}
+
+	car(car &x)
+	{
+		cout<<"INSIDE COPY CONSTRUCTOR"<<endl;
+		name=new char[strlen(x.name)+1];
+		strcpy(name,x.name);
+		model=x.model;
+		price=x.price;
+		seats=x.seats;
+	}
+
+	void print()
+	{
+		cout<<"name is "<<name<<endl;
+		cout<<"model is "<<model<<endl;
+		cout<<"seats are "<<seats<<endl;
+		cout<<"price is "<<price<<endl;
+	}
+
+	void setValues(char *ch,int m,int p,int s)
+	{
+		if(name!=NULL)
+		{
+			delete []name;
+			name=NULL;
+		}
+		name=new char[strlen(ch)+1];
+		strcpy(name,ch);
+		model=m;
+		price=p;
+		seats=s;
+	}
+
+	void setSeats(int s)
+	{
+		if(s>0)
+		{
+			seats=s;	
+		}
+		seats=4;	
+	}
+
+	int getSeats()
+	{
+		return seats;
+	}
+
+	void setName(char *ch)
+	{
+		if(name!=NULL)
+		{
+			delete []name;
+			name=NULL;
+		}
+		name=new char[strlen(ch)+1];
+		strcpy(name,ch);
+	}
+
+
+	void operator=(car x)
+	{
+		if(name!=NULL)
+		{
+			delete []name;
+			name=NULL;
+		}
+		name=new char[strlen(x.name)+1];
+		strcpy(name,x.name);
+		model=x.model;
+		price=x.price;
+		seats=x.seats;
+	}
+
+	void operator+=(car x)
+	{
+ 
+		char *oldname=name;
+		int l1=strlen(name);
+		int l2=strlen(x.name);
+		name=new char[l1+l2+1];
+		strcpy(name,oldname);
+		strcat(name,x.name);
+		delete []oldname;
+
+		model+=x.model;
+		price+=x.price;
+		seats+=x.seats;
+
+
+	}
+
+
+
+
+
+};
+
+
+int main()
+{
+	car A,B;
+	A.model=10;
+	A.price=1000;
+	// A.seats=4;
+	A.setSeats(-1);
+	A.setName("Audi");
+	// strcpy(A.name,"Audi");
+
+	B.setValues("BMW",12,950,4);
+
+	car C("honda",13,1001,4);
+
+	car D("maruti",14,100);
+
+//----COPY CONSTRUCTORS
+	car E(D);
+	car F=E;
+
+
+//--COPY ASSIGNMENT 
+	car G;
+	G=A;
+
+	// F.name[0]='G';
+
+	A+=B;
+
+	// cout<<"name is "<<A.name<<endl;
+	// cout<<"model is "<<A.model<<endl;
+	// cout<<"seats are "<<A.seats<<endl;
+	// cout<<"price is "<<A.price<<endl;
+	cout<<"seats are "<<A.getSeats()<<endl;
+	A.print();
+	cout<<endl;
+	B.print();
+	cout<<endl;
+	C.print();
+	cout<<endl;
+	D.print();
+	cout<<endl;
+	E.print();
+	cout<<endl;
+	F.print();
+	cout<<endl;
+	G.print();
+	cout<<endl;
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+
