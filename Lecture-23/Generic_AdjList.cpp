61cf5e680c919201cc613d2593d5b1e821f4d349
kartik <kartik.mathur10@gmail.com>
2019-07-29 10:33:35 +0530

    Lecture-24

--- Lecture-23/Generic_AdjList.cpp
+++ Lecture-23/Generic_AdjList.cpp
@@ -72,26 +72,83 @@ public:
 		cout<<endl<<"Number of components : "<<component<<endl;
 	}
 
+	int SSSP(T src,T des){
+		unordered_map<T,T> parent_map;
+		map<T,int> dist;
+		queue<T> q;
+		for(auto node:h){
+			dist[node.first] = INT_MAX;
+		}
+		dist[src] = 0;
+		parent_map[src]=src;
+		
+		q.push(src);
+		while(!q.empty()){
+			T node = q.front();
+			q.pop();
+
+			for(auto children:h[node]){
+				if(dist[children]==INT_MAX){
+					dist[children]=dist[node]+1;
+					q.push(children);
+					parent_map[children] = node;
+				}
+			}
+		}
+
+		T temp = des;
+		while(temp!=src){
+			cout<<temp<<"<--";
+			temp=parent_map[temp];
+		}
+		cout<<src<<endl;
+
+		// for(auto node:dist){
+		// 	cout<<"Distance of "<<node.first<<" from "<<src<<" is "<<dist[node.first]<<endl;
+		// }
+		return dist[des];
+	}
+
 };
 
 int main(){
 	// Graph<string> g;
 	Graph<int> g;
+	int board[50] = {0};
+	board[2] = 13;
+	board[5] = 2;
+	board[9] = 18;
+	board[18] = 11;
+	board[17]=-13;
+	board[20] = -14;
+	board[24] = -8;
+	board[25] = -10;
+	board[32] = -2;
+	board[34] = -22;
 
-	g.addEdge(0,1);
-	g.addEdge(2,1);
-	g.addEdge(2,4);
-	g.addEdge(0,4);
-	g.addEdge(3,4);
-	g.addEdge(3,2);
-	g.addEdge(3,5);
-	g.addEdge(30,50);
-	g.addEdge(30,51);
-	g.addEdge(500,20);
-	g.addEdge(21,500);
+
+	for(int u=0;u<=36;u++){
+		for(int dice = 1;dice<=6;dice++){
+			int v = u+dice+board[u+dice];
+			g.addEdge(u,v,false);
+		}
+	}
+
+
+	// g.addEdge(0,1);
+	// g.addEdge(2,1);
+	// g.addEdge(2,4);
+	// g.addEdge(0,4);
+	// g.addEdge(3,4);
+	// g.addEdge(3,2);
+	// g.addEdge(3,5);
+	// g.addEdge(30,50);
+	// g.addEdge(30,51);
+	// g.addEdge(500,20);
+	// g.addEdge(21,500);
 
 	// g.BFS(0);
-	g.DFS(0);
+	cout<<g.SSSP(0,36)<<endl;
 
 
 
